2568704bf900ddb2044ec3f3336effbe30dc7bcb
SaiPawar12345 <163424284+SaiPawar12345@users.noreply.github.com>
2024-10-22 10:33:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Cart from './components/Cart';
+import Products from './components/Products'; // Import Products component
+import Navbar from './components/Navbar'; // Import Navbar component
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        {/* You can add the Navbar here to make it visible on all pages */}
+        {/* Define routes */}
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home page */}
+          <Route path="/home" element={<Home />} /> {/* Home page */}
+          <Route path="/cart" element={<Cart />} /> {/* Cart page */}
+          <Route path="/products" element={<Products />} /> {/* Products page */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
